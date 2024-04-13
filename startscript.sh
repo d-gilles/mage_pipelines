1a9074f966fdd886590fdce048ad4e595e639a86
David Gilles <info@davidgilles.de>
2024-04-13 12:51:11 +0200

    first commands

--- startscript.sh
+++ startscript.sh
@@ -1,1 +1,12 @@
-touch hereiam.txt
+cd ..
+
+apt-get update && apt-get install -y git openssh-client
+mkdir -p ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+rm -rf scr 
+git clone https://github.com/d-gilles/mage_pipelines.git
+mv mage_pipelines scr
+
+cd scr
+pip install -r requirements.txt
