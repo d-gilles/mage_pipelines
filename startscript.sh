83809ef02808c26491de8253cd1bb811aec18def
David Gilles <114345154+d-gilles@users.noreply.github.com>
2024-04-13 14:16:11 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -1,11 +1,3 @@
 cd ..
 
-apt-get update
 
-rm -rf src 
-git clone https://github.com/d-gilles/mage_pipelines.git
-mv mage_pipelines src
-
-cd scr
-pip install --upgrade pip
-pip install -r requirements.txt
